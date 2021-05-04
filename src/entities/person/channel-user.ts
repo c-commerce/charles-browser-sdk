67c6eea46e139edc90a493199f4b9449e2e8257f
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2021-05-04 12:44:10 +0200

    feat(Notification): add extra params
    Signed-off-by: Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>

--- src/entities/person/channel-user.ts
+++ src/entities/person/channel-user.ts
@@ -143,14 +143,15 @@ export class ChannelUser {
     }
   }
 
-  public async sendMessageFromMessageTemplate (messageTemplate: messageTemplate.MessageTemplate, language: string, parameters?: object | object[] | null): Promise<event.Event | undefined> {
+  public async sendMessageFromMessageTemplate (messageTemplate: messageTemplate.MessageTemplate, language: string, parameters?: object | object[] | null, extra?: {[key: string]: any}): Promise<event.Event | undefined> {
     try {
       const opts = {
         method: 'POST',
         url: `${this.universe.universeBase}/api/v0/people/${this.person as string}/channel_users/${this.id as string}/notifications/templates/${messageTemplate.id as string}`,
         data: {
           parameters,
-          language
+          language,
+          ...(extra ?? {})
         }
       }
       const response = await this.http.getClient()(opts)
