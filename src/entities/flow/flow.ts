be11f4fb3297c332234f05795af8bbb38e44e6b5
Mohab Amr <mohabamr1@gmail.com>
2023-09-26 20:58:29 +0300

    feat: fetch top performing flows
    fix: adding trigger type to flows

---
+++ src/entities/flow/flow.ts
@@ -0,0 +1,12 @@
+
+export type TriggerType = 'SHOP_TRIGGER' | 'MESSAGE_KEYWORD_TRIGGER' | 'MESSAGE_EXACT_TRIGGER' | 'REST_TRIGGER' | 'MANUAL_TRIGGER' | 'KLAVIYO_TRIGGER'
+export interface FlowRawPayload {
+  flowId: string
+  flowName: string
+  revenue: number
+  triggered: number
+  source?: string
+  clicks?: number
+  revenuePerTrigger?: number
+  triggerType?: TriggerType
+}
