3dfcc8707ad1e119d3e5dfee263dfd876b98583e
Artem Danilov <92536895+artdnlv@users.noreply.github.com>
2024-01-04 11:31:43 +0100

    feat: remove examples, refactor github action

--- examples/vue/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-notes/register'
-  ],
-}
